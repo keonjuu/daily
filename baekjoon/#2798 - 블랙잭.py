45247b9229b9bd4e54c0a459ed2c881333a697fd
keonjuu <56825463+keonjuu@users.noreply.github.com>
2019-10-27 21:59:59 +0900

    Add files via upload

---
+++ baekjoon/#2798 - 블랙잭.py
@@ -0,0 +1,18 @@
+# 5 21
+# 5 6 7 8 9
+import sys
+
+def blackjack(n,m,card)
+  maxCard = 0
+  for c1 in range(n-2)
+    for c2 in range(c1+1, n-1)
+      for c3 in range(c2+1, n)
+        if maxCard == m return maxCard 
+        if card[c1]+card[c2]+card[c3]=m and maxCard  (card[c1] +card[c2] +card[c3])
+            maxCard = card[c1] +card[c2] +card[c3]
+
+  return maxCard 
+
+n,m = map(int, sys.stdin.readline().split()) 
+card = list(map(int, sys.stdin.readline().split())) 
+print(blackjack(n,m,card))
